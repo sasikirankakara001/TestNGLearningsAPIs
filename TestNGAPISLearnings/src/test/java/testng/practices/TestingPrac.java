5c07e6b34f2d3d8733cb5d0f49752736c294280f
SasikiranKakara <55825519+sasikirankakara001@users.noreply.github.com>
2023-10-15 22:42:13 +0530

    commit

--- TestNGAPISLearnings/src/test/java/testng/practices/TestingPrac.java
+++ TestNGAPISLearnings/src/test/java/testng/practices/TestingPrac.java
@@ -71,5 +71,24 @@ public class TestingPrac {
 				+ "").when().put("maps/api/place/update/json").then().spec(resp).log().all().extract().asString();
 		
 	}
+	@Test
+	public void googleMapsAPIsDelete() throws Exception {
+		String extractDetails = given().spec(req)
+				.body("{\r\n" + "  \"location\": {\r\n" + "    \"lat\": -38.383494,\r\n" + "    \"lng\": 33.427362\r\n"
+						+ "  },\r\n" + "  \"accuracy\": 50,\r\n" + "  \"name\": \"Frontline house\",\r\n"
+						+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
+						+ "  \"address\": \"29, side layout, cohen 09\",\r\n" + "  \"types\": [\r\n"
+						+ "    \"shoe park\",\r\n" + "    \"shop\"\r\n" + "  ],\r\n"
+						+ "  \"website\": \"http://google.com\",\r\n" + "  \"language\": \"French-IN\"\r\n" + "}\r\n"
+						+ "\r\n" + "")
+				.when().post("maps/api/place/add/json").then().spec(resp).extract().asString();
+		JsonPath jsonPath = new JsonPath(extractDetails);
+		String placeId = jsonPath.getString("place_id");
+		given().spec(req).body("{\r\n"
+				+ "    \"place_id\":\""+placeId+"\"\r\n"
+				+ "}\r\n"
+				+ "").when().delete("maps/api/place/delete/json").then().spec(resp).log().all().extract().asString();
+		
+	}
 
 }
